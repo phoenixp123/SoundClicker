6962d8916b38e0f8e703a0fdb06e67a68606803e
phoenixp123 <68199764+phoenixp123@users.noreply.github.com>
2020-08-12 11:56:33 -0400

    Update Listener.py

--- Listener.py
+++ Listener.py
@@ -4,13 +4,16 @@ from array import array
 import numpy as np
 import librosa
 import soundfile
-from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import accuracy_score
+from sklearn.svm import OneClassSVM
+from sklearn.model_selection import train_test_split, cross_val_score
 import os
+from pynput.mouse import Button,Controller
 
-sound = {'00': 'click_failed','01': 'click'}
 
-
-def record():
+def record(deployed):
     """This function records incoming audio by first running for a set
     time; if the user speaks above the set volume threshold then append
     the audio data to an array called frames. If the user has already
@@ -37,26 +40,29 @@ def record():
         data = stream.read(CHUNK,exception_on_overflow = False)
         data_chunk = array('h',data)
         vol = max(data_chunk)
-        if vol >= 4000:
+        if vol >= 3500:
             check_state = True
             frames.append(data)
-            print("something said")
-        elif check_state == True and vol < 4000:
+            if not deployed:
+                print("something said")
+        elif check_state == True and vol < 3000:
             break
         else:
             check_state = False
-            print("nothing")
-        print("\n")
+            if not deployed:
+                print("nothing")
+        if not deployed:
+            print("\n")
 
     sample_width = audio.get_sample_size(FORMAT)
     return sample_width,frames,CHANNELS,RATE
 
 
-def file_writer(FILE_NAME):
+def file_writer(FILE_NAME,deployed):
     """retrieves the sample width, array of audio data, and record settings
     as they are returned from record(). The function uses that information to write
     a wav file with that data"""
-    sample_width,frames,CHANNELS,RATE = record()
+    sample_width,frames,CHANNELS,RATE = record(deployed)
     wf = wave.open(FILE_NAME,'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(sample_width)
@@ -72,8 +78,7 @@ def extract_features(file_name,mfcc,chroma,mel):
     with soundfile.SoundFile(file_name) as audio_data:
         x = audio_data.read(dtype = "float32")
         sample_rate = audio_data.samplerate
-        if chroma:
-            stft = np.absolute(librosa.stft(x))
+        stft = np.absolute(librosa.stft(x))
         features = np.array([])
         if mfcc:
             mfccs = np.mean(librosa.feature.mfcc(y = x,sr = sample_rate,n_mfcc = 40).T,axis = 0)
@@ -87,59 +92,58 @@ def extract_features(file_name,mfcc,chroma,mel):
     return features
 
 
-def build_dataset(i):
+def build_dataset(testing):
     """creates two numPy arrays, containing the features and label of an audio
     recording, respectively."""
-    y = np.array([])
-    FILE_NAME = "recording%d.wav" % i
-    file_writer(FILE_NAME)
-    X = extract_features(FILE_NAME,mfcc = True,chroma = True,mel = True)
-    y = np.append(y,sound['01'])
+    FILE_NAME = "recording.wav"
+    file_writer(FILE_NAME,testing)
+    X = extract_features(FILE_NAME,mfcc = True,chroma = False,mel = False)
     os.remove(FILE_NAME)
-    return X,y
+    return X
 
 
-def get_training_data(samples):
+def get_training_data(samples,state):
     """This function returns the complete arrays of observations and
     labels, respectively."""
-    observations = np.empty((0,180))
-    labels = np.empty((0,1))
+    observations = np.empty((0,40))
     for i in range(samples):
-        X,y = build_dataset(i)
-        examples,target = np.array([X]),np.array([y])
+        X = build_dataset(state)
+        examples = np.array([X])
         observations = np.append(observations,examples,axis = 0)
-        labels = np.append(labels,target,axis = 0)
-    observations.reshape(1,-1),labels.reshape(1,-1)
-    return observations,labels
+    observations.reshape(1,-1)
+    return observations
 
 
-def get_sample(activation):
-    X_test = np.empty((0,180))
-    filename = "test%d.wav" % activation
-    file_writer(filename)
-    new_sample = extract_features(filename,mfcc = True,chroma = True,mel = True)
+def get_sample():
+    X_test = np.empty((0,40))
+    filename = "test.wav"
+    file_writer(filename,deployed = True)
+    new_sample = extract_features(filename,mfcc = True,chroma = False,mel = False)
     inference = np.array([new_sample])
-    X_test = np.append(X_test, inference, axis = 0)
+    X_test = np.append(X_test,inference,axis = 0)
     os.remove(filename)
     return X_test
 
 
+def generate_data(test_size):
+    synthetic_data = np.empty((0,40))
+    X_train = get_training_data(test_size,False)
+    for _ in range(1000):
+        # create dataset with a degree of variation
+        synthetic_data = np.append(synthetic_data, X_train, axis = 0)
+    return synthetic_data
+
+
 def deploy_model(test_size):
-    clf = MLPClassifier(random_state = 1, batch_size = 100)
-    X_train,y_train = get_training_data(test_size)
-    clf.fit(X_train,y_train.ravel())
-    activations = 0
+    mouse = Controller()
+    clf = OneClassSVM(kernel = 'rbf')
+    X = generate_data(test_size)
+    clf.fit(X)
     while True:
-        X_predict = get_sample(activations)
-        if clf.predict(X_predict) == sound['01']:
+        X_predict = get_sample()
+        if clf.predict(X_predict) == 1:
             print('Click!')
-            # generate_click
+            mouse.click(Button.left,1)
         else:
-            print('not click:(')
+            print("failed")
         print("\n")
-        activations += 1
-
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
